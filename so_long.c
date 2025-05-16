52f0720f2ae7b31531823935f688280ad5055aa7
Donghyun <hdh1324@gmail.com>
2025-05-16 14:19:20 +0300

    delete unnecessary code

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: donheo <donheo@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 11:40:41 by donheo            #+#    #+#             */
-/*   Updated: 2025/05/14 11:36:51 by donheo           ###   ########.fr       */
+/*   Updated: 2025/05/16 14:18:58 by donheo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,5 @@ int	main(int ac, char **av)
 	map = check_map_validation(av[1]);
 
 	free_map(map);
-	if (map != 0 && ac == 0)
-		return (0);
 	return (0);
 }
