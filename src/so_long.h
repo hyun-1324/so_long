7315f3c1590c4930e245d25733a737abe8ac3bc0
Donghyun <hdh1324@gmail.com>
2025-05-22 16:36:33 +0300

    add comments for header

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: donheo <donheo@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/13 11:37:43 by donheo            #+#    #+#             */
-/*   Updated: 2025/05/20 23:13:14 by donheo           ###   ########.fr       */
+/*   Updated: 2025/05/22 16:36:16 by donheo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,19 @@
 # include "../libft/libft.h"
 # include "../MLX42/include/MLX42/MLX42.h"
 
+// Constants for rendering
 # define TILE_SIZE 64
 # define MONITOR_WIDTH 3840
 # define MONITOR_LENGTH 2048
+
+// Texture paths
 # define WALL "./textures/wall.png"
 # define FLOOR "./textures/floor.png"
 # define COLLECTIBLE "./textures/collectible.png"
 # define EXIT "./textures/exit.png"
 # define PLAYER "./textures/player.png"
 
+// Struct to store map information
 typedef struct s_map
 {
 	int		length;
@@ -36,6 +40,7 @@ typedef struct s_map
 	char	**content;
 }	t_map;
 
+// Struct to store game state and resources
 typedef struct s_game
 {
 	t_map		*map;
@@ -49,18 +54,21 @@ typedef struct s_game
 	int			collectibles;
 }	t_game;
 
+// Core functions and memory/error handling
 int		main(int ac, char **av);
 void	print_err(char *str);
-void	init_map(t_map *map);
 void	free_map(t_map *map);
 void	free_content(char **content);
-char	*without_next_line(char *str, int *last_line);
 
+// Map validation helpers
 t_map	*check_map_validation(char *map_name);
+char	*without_next_line(char *str, int *last_line);
 char	*get_next_line(int fd, t_map *map);
+void	init_map(t_map *map);
 int		check_chars(t_map *map, int exit_check, int player_check);
 int		check_path(t_map *map);
 
+// Rendering and gameplay logic
 void	handle_key_presses(mlx_key_data_t key_data, void *param);
 void	render_objects(t_game *game);
 void	render_background(t_game *game);
